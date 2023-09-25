10d634234dc1efc1b60c993b975d625974929458
didszun <didszun@outlook.com>
2023-09-25 15:33:51 +0200

    app bar added to map view

--- lib/mod_map_example.dart
+++ lib/mod_map_example.dart
@@ -19,63 +19,64 @@ class _MapExampleState extends State<MapExample> {
   }
 
   Future<void> fetchData() async {
-  /*  //final String? userId = ModalRoute.of(context)!.settings.arguments as String?;
-
-    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users/1'));
-
-    if (response.statusCode == 200) {
-      setState(() {
-        // ...
-      });
-    } else {
-      throw Exception('Failed to load data');
-    }*/
   }
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        center: LatLng(49.791560300794615, 9.945795763150798),
-        zoom: 18,
+    return Scaffold(
+      appBar: AppBar(
+          leading: const Icon(Icons.menu),
+          title: const Text("Flutter-Maps-Example"),
+          backgroundColor: Colors.orange
+
       ),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'org.didszun.app',
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(49.791560300794615, 9.945795763150798),
+          zoom: 18,
+          maxZoom: 18, // in OSM maximaler Zoom
+
         ),
-        MarkerLayer(
-          markers: [
-            Marker(
-              point: LatLng(49.791560300794615, 9.945795763150798),
-              width: 80,
-              height: 80,
-              builder: (context) => const Icon(
-                  Icons.close_fullscreen,
-                  color: Colors.deepOrange,
-                  shadows: <Shadow>[Shadow(color: Colors.white, blurRadius: 5.0)],
-                  size: 25), // Icon() FlutterLogo(),
-            ),
-          ],
-        ),
-        PolygonLayer(
-          polygons: [
-            Polygon(
-              points: [
-                LatLng(49.7921622630431, 9.94494787530687),
-                LatLng(49.79195424390552, 9.94560665412181),
-                LatLng(49.792134527209704, 9.946329878690388),
-                LatLng(49.792402639600425, 9.945363192385855),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'org.didszun.app',
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(49.791560300794615, 9.945795763150798),
+                width: 80,
+                height: 80,
+                builder: (context) =>
+                const Icon(
+                    Icons.close_fullscreen,
+                    color: Colors.deepOrange,
+                    shadows: <Shadow>[
+                      Shadow(color: Colors.white, blurRadius: 5.0)
+                    ],
+                    size: 25), // Icon() FlutterLogo(),
+              ),
+            ],
+          ),
+          PolygonLayer(
+            polygons: [
+              Polygon(
+                points: [
+                  LatLng(49.7921622630431, 9.94494787530687),
+                  LatLng(49.79195424390552, 9.94560665412181),
+                  LatLng(49.792134527209704, 9.946329878690388),
+                  LatLng(49.792402639600425, 9.945363192385855),
                 ],
-              color: Colors.lightBlueAccent.withOpacity(0.5),
-              isFilled: true,
-              isDotted: true,
-              borderColor: Colors.blue,
-            ),
-          ],
-        ),
-      ],
+                color: Colors.lightBlueAccent.withOpacity(0.5),
+                isFilled: true,
+                isDotted: true,
+                borderColor: Colors.blue,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
-
